c09c4be3dab8cb7c5b6aa9a6456b3a182773e2f9
Arthur Bridges <arthurbridges08@yahoo.com>
2023-12-18 16:39:57 -0600

    swapping from handlebars -> ejs

--- server.js
+++ server.js
@@ -1,23 +1,18 @@
-// CHANGING FROM HANDLEBARS --> EJS
 const path = require('path');
 const express = require('express');
 const session = require('express-session');
-const exphbs = require('express-handlebars');
 const SequelizeStore = require('connect-session-sequelize')(session.Store);
 
 const routes = require('./controllers');
 const sequelize = require('./config/connection');
-const helpers = require('./utils/helpers');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
 
 // Set up sessions with cookies
-//TODO: finish setting up cookies in server.js
 const sess = {
   secret: process.env.SESSION_SECRET,
   cookie: {
-    // Stored in milliseconds
     maxAge: 24 * 60 * 60 * 1000, // expires after 1 day
   },
   resave: false,
@@ -29,10 +24,9 @@ const sess = {
 
 app.use(session(sess));
 
-const hbs = exphbs.create({ helpers });
-
-app.engine('handlebars', hbs.engine);
-app.set('view engine', 'handlebars');
+// Set EJS as the view engine
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views')); // if your EJS files are in a 'views' folder
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
@@ -43,6 +37,6 @@ app.use(routes);
 sequelize.sync({ force: false }).then(() => {
   app.listen(PORT, () =>
     console.log(`
-            \nServer running on port ${PORT}.Visit http://localhost:${PORT} and create an account!`),
+            \nServer running on port ${PORT}. Visit http://localhost:${PORT} and create an account!`),
   );
 });
